91d52ec8d90eee627d7883510c5ba5cc31e8bd80
thegicode <thegi.code@gmail.com>
2023-12-04 13:09:18 +0900

    rename format to usd

--- src/chp01/statement-2.js
+++ src/chp01/statement-2.js
@@ -11,13 +11,13 @@ function statement(invoice) {
         volumeCredits += volumeCreditsFor(perf);
 
         // 청구 내역을 출력한다.
-        result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
             perf.audience
         }석)\n`;
         totalAmount += amountFor(perf);
     }
 
-    result += `총액: ${format(totalAmount / 100)}\n`;
+    result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
 }
@@ -59,10 +59,10 @@ function volumeCreditsFor(aPerformance) {
     return volumeCredits;
 }
 
-function format(aNumber) {
+function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
         style: "currency",
         currency: "USD",
         minimumFractionDigits: 2,
-    }).format(aNumber);
+    }).format(aNumber / 100);
 }

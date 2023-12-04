0cb737d88709cd9c47f28c040f53d20d4db934db
thegicode <thegi.code@gmail.com>
2023-12-04 12:59:34 +0900

    update statement

--- src/chp01/statement-2.js
+++ src/chp01/statement-2.js
@@ -1,7 +1,12 @@
-function statement(invoice, plays) {
+const plays = require("./plays.json");
+const invoices = require("./invoices.json");
+
+console.log(statement(invoices));
+
+function statement(invoice) {
     let totalAmount = 0;
     let volumeCredits = 0;
-    let result = `청구 내역 (고객명: ${invoice.customer}\n`;
+    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
     const format = new Intl.NumberFormat("en-US", {
         style: "currency",
         currency: "USD",
@@ -9,22 +14,18 @@ function statement(invoice, plays) {
     }).format;
 
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
-
-        let thisAmount = amountFor(perf, lay); // 추출한 함수를 이용
-
         // 포인트를 적립한다.
         volumeCredits += Math.max(perf.audience - 30, 0);
 
         // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if ("comedy" === play.type)
+        if ("comedy" === playFor(perf).type)
             volumeCredits += Math.floor(perf.audience / 5);
 
         // 청구 내역을 출력한다.
-        result += ` ${paly.name}: ${format(thisAmount / 100)} (${
+        result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
             perf.audience
-        }석\n)`;
-        totalAmount += thisAmount;
+        }석)\n`;
+        totalAmount += amountFor(perf);
     }
 
     result += `총액: ${format(totalAmount / 100)}\n`;
@@ -32,27 +33,31 @@ function statement(invoice, plays) {
     return result;
 }
 
-function amountFor(perf, play) {
+function amountFor(aPerformance) {
     // 값이 바뀌지 않는 변수는 매개변수로 전달
-    let thisAmount = 0; // 변수를 초기화하는 코드
+    let result = 0; // 변수를 초기화하는 코드
 
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
         case "tragedy": // 비극
-            thisAmount = 400000;
-            if (perf.audience > 30) {
-                thisAmount += 1000 * (perf.audience - 30);
+            result = 40000;
+            if (aPerformance.audience > 30) {
+                result += 1000 * (aPerformance.audience - 30);
             }
             break;
         case "comedy": // 희극
-            thisAmount = 300000;
-            if (perf.audience > 20) {
-                thisAmount += 1000 + 500 * (perf.audience - 20);
+            result = 30000;
+            if (aPerformance.audience > 20) {
+                result += 1000 + 500 * (aPerformance.audience - 20);
             }
-            thisAmount += 300 * perf.audience;
+            result += 300 * aPerformance.audience;
             break;
         default:
-            throw new Error(`알 수 없는 장르: ${paly.type}`);
+            throw new Error(`알 수 없는 장르: ${playFor(perf).type}`);
     }
 
-    return thisAmount; // 함수 안에서 값이 바뀌는 함수 반환
+    return result; // 함수 안에서 값이 바뀌는 함수 반환
+}
+
+function playFor(aPerformance) {
+    return plays[aPerformance.playID];
 }
--- src/chp01/statement.js
+++ src/chp01/statement.js
@@ -1,7 +1,12 @@
+const plays = require("./plays.json");
+const invoice = require("./invoices.json");
+
+console.log(statement(invoice, plays));
+
 function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
-    let result = `청구 내역 (고객명: ${invoice.customer}\n`;
+    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
     const format = new Intl.NumberFormat("en-US", {
         style: "currency",
         currency: "USD",
@@ -14,20 +19,20 @@ function statement(invoice, plays) {
 
         switch (play.type) {
             case "tragedy": // 비극
-                thisAmount = 400000;
+                thisAmount = 40000;
                 if (perf.audience > 30) {
                     thisAmount += 1000 * (perf.audience - 30);
                 }
                 break;
             case "comedy": // 희극
-                thisAmount = 300000;
+                thisAmount = 30000;
                 if (perf.audience > 20) {
                     thisAmount += 1000 + 500 * (perf.audience - 20);
                 }
                 thisAmount += 300 * perf.audience;
                 break;
             default:
-                throw new Error(`알 수 없는 장르: ${paly.type}`);
+                throw new Error(`알 수 없는 장르: ${play.type}`);
         }
 
         // 포인트를 적립한다.
@@ -38,9 +43,9 @@ function statement(invoice, plays) {
             volumeCredits += Math.floor(perf.audience / 5);
 
         // 청구 내역을 출력한다.
-        result += ` ${paly.name}: ${format(thisAmount / 100)} (${
+        result += ` ${play.name}: ${format(thisAmount / 100)} (${
             perf.audience
-        }석\n)`;
+        }석)\n`;
         totalAmount += thisAmount;
     }
 

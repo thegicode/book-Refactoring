f5bd446bcf0343c428ed7483ff246ac360a0967b
thegicode <thegi.code@gmail.com>
2023-12-04 13:06:26 +0900

    update statement: add foramt

--- src/chp01/statement-2.js
+++ src/chp01/statement-2.js
@@ -7,12 +7,6 @@ function statement(invoice) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-    const format = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 2,
-    }).format;
-
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
 
@@ -63,4 +57,12 @@ function volumeCreditsFor(aPerformance) {
     if ("comedy" === playFor(aPerformance).type)
         volumeCredits += Math.floor(aPerformance.audience / 5);
     return volumeCredits;
+}
+
+function format(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2,
+    }).format(aNumber);
 }

ae7d2c8e2f5b4b9c0fa6e094f373d5a1116bf11f
thegicode <thegi.code@gmail.com>
2023-12-05 11:09:33 +0900

    update volumeCredit

--- src/chp01/statement-2.js
+++ src/chp01/statement-2.js
@@ -13,7 +13,16 @@ function statement(invoice, plays) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
         result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
         return result;
+    }
+
+    function volumeCreditsFor(aPerformance) {
+        let volumeCredits = 0;
+        volumeCredits += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === aPerformance.play.type)
+            volumeCredits += Math.floor(aPerformance.audience / 5);
+        return volumeCredits;
     }
 
     function amountFor(aPerformance) {
@@ -44,7 +53,7 @@ function statement(invoice, plays) {
         return plays[aPerformance.playID];
     }
 
-    function renderPlainText(data, plays) {
+    function renderPlainText(data) {
         let result = `청구 내역 (고객명: ${data.customer})\n`;
 
         for (let perf of data.performances) {
@@ -68,7 +77,7 @@ function statement(invoice, plays) {
         function totalVolumeCredits() {
             let result = 0;
             for (let perf of data.performances) {
-                result += volumeCreditsFor(perf);
+                result += perf.volumeCredits;
             }
             return result;
         }
@@ -79,14 +88,6 @@ function statement(invoice, plays) {
                 currency: "USD",
                 minimumFractionDigits: 2,
             }).format(aNumber / 100);
-        }
-
-        function volumeCreditsFor(aPerformance) {
-            let volumeCredits = 0;
-            volumeCredits += Math.max(aPerformance.audience - 30, 0);
-            if ("comedy" === aPerformance.play.type)
-                volumeCredits += Math.floor(aPerformance.audience / 5);
-            return volumeCredits;
         }
     }
 }

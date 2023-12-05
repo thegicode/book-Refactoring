cc0ad3add854ce120aecd4c25613734e103d3f80
thegicode <thegi.code@gmail.com>
2023-12-05 11:46:40 +0900

    update html

--- src/chp01/statement.js
+++ src/chp01/statement.js
@@ -2,21 +2,24 @@ import createStatementData from "./createStatementData.js";
 import plays from "./plays.json" assert { type: "json" };
 import invoices from "./invoices.json" assert { type: "json" };
 
-function statement(invoice, plays) {
-    return renderPlainText(createStatementData(invoice, plays));
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
 }
 
-function renderPlainText(data) {
-    let result = `청구 내역 (고객명: ${data.customer})\n`;
+function renderHtml(data) {
+    let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>\n";
 
     for (let perf of data.performances) {
-        result += ` ${perf.play.name}: ${usd(perf.amount)} (${
-            perf.audience
-        }석)\n`;
+        result += `<tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
     }
 
-    result += `총액: ${usd(data.totalAmount)}\n`;
-    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+    result += "</table>\n";
+
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
     return result;
 
     function usd(aNumber) {
@@ -28,4 +31,4 @@ function renderPlainText(data) {
     }
 }
 
-console.log(statement(invoices, plays));
+console.log(htmlStatement(invoices, plays));

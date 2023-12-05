1aabb26fa653e20e85365a37ec8d1e18e777108d
thegicode <thegi.code@gmail.com>
2023-12-05 10:57:52 +0900

    update data

--- src/chp01/statement-2.js
+++ src/chp01/statement-2.js
@@ -6,14 +6,24 @@ console.log(statement(invoices, plays));
 function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances;
+    statementData.performances = invoice.performances.map(enrichPerformance);
     return renderPlainText(statementData, plays);
+
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance);
+        result.play = playFor(result);
+        return result;
+    }
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
 
     function renderPlainText(data, plays) {
         let result = `청구 내역 (고객명: ${data.customer})\n`;
 
         for (let perf of data.performances) {
-            result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
+            result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${
                 perf.audience
             }석)\n`;
         }
@@ -49,20 +59,16 @@ function statement(invoice, plays) {
         function volumeCreditsFor(aPerformance) {
             let volumeCredits = 0;
             volumeCredits += Math.max(aPerformance.audience - 30, 0);
-            if ("comedy" === playFor(aPerformance).type)
+            if ("comedy" === aPerformance.play.type)
                 volumeCredits += Math.floor(aPerformance.audience / 5);
             return volumeCredits;
-        }
-
-        function playFor(aPerformance) {
-            return plays[aPerformance.playID];
         }
 
         function amountFor(aPerformance) {
             // 값이 바뀌지 않는 변수는 매개변수로 전달
             let result = 0; // 변수를 초기화하는 코드
 
-            switch (playFor(aPerformance).type) {
+            switch (aPerformance.play.type) {
                 case "tragedy": // 비극
                     result = 40000;
                     if (aPerformance.audience > 30) {
@@ -77,7 +83,9 @@ function statement(invoice, plays) {
                     result += 300 * aPerformance.audience;
                     break;
                 default:
-                    throw new Error(`알 수 없는 장르: ${playFor(perf).type}`);
+                    throw new Error(
+                        `알 수 없는 장르: ${aPerformance.play.type}`
+                    );
             }
 
             return result; // 함수 안에서 값이 바뀌는 함수 반환

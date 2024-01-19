760ce3964d9cd5d03ddcd2d529daf6e34005f199
thegicode <thegi.code@gmail.com>
2024-01-19 19:11:50 +0900

    chap 8.2.2

---
+++ src/chp08/MoveField2.js
@@ -0,0 +1,41 @@
+/* export class Account {
+    constructor(number, type, interestRate) {
+        this._number = number;
+        this._type = type;
+        this._interestRate = interestRate;
+    }
+
+    get interestRate() {
+        return this._interestRate;
+    }
+}
+
+export class AccountType {
+    constructor(nameString) {
+        this._name = nameString;
+    }
+} */
+
+export class Account {
+    constructor(number, type) {
+        this._number = number;
+        this._type = type;
+        // assert((insterstRate = this._type.insterstRate));
+        // this._interestRate = interestRate;
+    }
+
+    get interestRate() {
+        return this._type.interestRate;
+    }
+}
+
+export class AccountType {
+    constructor(nameString, interestRate) {
+        this._name = nameString;
+        this._interestRate = interestRate;
+    }
+
+    get interestRate() {
+        return this._interestRate;
+    }
+}
